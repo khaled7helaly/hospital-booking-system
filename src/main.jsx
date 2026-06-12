2af2b8076dab5cb114737aaf580357d09612c867
khaled helaly <helalykhaled3@gmail.com>
2026-06-12 20:04:59 +0300

    Fix router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Router>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router basename="/hospital-booking-system">
       <App />
     </Router>
-  </StrictMode>
+  </React.StrictMode>
 );
